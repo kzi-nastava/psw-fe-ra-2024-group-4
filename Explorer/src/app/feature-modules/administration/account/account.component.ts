adecf62080876b19a48dd9735e8f9c49e318fb14
stasaaa <radojicic.stasa@gmail.com>
2024-10-16 22:18:36 +0200

    feat: implement user blocking; hide password from admin; exlude logged-in admin from account list

--- Explorer/src/app/feature-modules/administration/account/account.component.ts
+++ Explorer/src/app/feature-modules/administration/account/account.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Account } from '../model/account.model';
 import { AdministrationService } from '../administration.service';
 import { PagedResults } from 'src/app/shared/model/paged-results.model';
+import { User } from 'src/app/infrastructure/auth/model/user.model';
+import { AuthService } from 'src/app/infrastructure/auth/auth.service';
 
 @Component({
   selector: 'xp-account',
@@ -11,17 +13,41 @@ import { PagedResults } from 'src/app/shared/model/paged-results.model';
 export class AccountComponent implements OnInit {
 
   account: Account[] = []
+  user: User | undefined;
 
-  constructor(private service: AdministrationService) { }
+  constructor(private service: AdministrationService, private authService: AuthService) { }
 
   ngOnInit(): void {
+      this.authService.user$.subscribe(user => {
+        this.user = user;
+      });
+
       this.service.getAccount().subscribe({
         next: (result: PagedResults<Account>) => {
           this.account = result.results;
+          if (this.user) {
+            this.account = this.account.filter(item => item.id !== this.user?.id);
+          }
+          this.account.forEach(ac => {
+            ac.password = '*'.repeat(ac.password.length);
+          });
         },
         error: (err: any) => {
           console.log(err);
         }
       })
+  }
+
+  onBlockClicked(ac: Account): void {
+    this.service.blockAccount(ac).subscribe({
+      next: (result: Account) => {
+        this.account = this.account.map(item => 
+          item.id === ac.id ? result : item
+        );
+      },
+      error: (err: any) => {
+        console.log(err);
+      }
+    })
   }
 }
