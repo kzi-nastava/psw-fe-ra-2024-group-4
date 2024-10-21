417b41a30c26edb13b687cb3dc1a4b1d25164ae4
stasaaa <radojicic.stasa@gmail.com>
2024-10-21 20:32:37 +0200

    css for landing page blog component improved

--- Explorer/src/app/feature-modules/layout/landing-blog/landing-blog.component.ts
+++ Explorer/src/app/feature-modules/layout/landing-blog/landing-blog.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Post } from '../../blog/model/post.model';
+import { Post, Status } from '../../blog/model/post.model';
 import { CommentService } from '../../blog/comment.service';
 import { User } from 'src/app/infrastructure/auth/model/user.model';
 import { AuthService } from 'src/app/infrastructure/auth/auth.service';
@@ -13,22 +13,25 @@ import { PagedResults } from 'src/app/shared/model/paged-results.model';
 export class LandingBlogComponent implements OnInit {
 
   genericImage: string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQl6raU10dijlmB2SobtTOlXvlwOe55tE0LjQ&s"
-  blogs: Post[];
-  mainBlog: Post;
+  blogs: Post[] = [];
+  mainBlog: Post | undefined;
   user: User | undefined;
 
+  /*
   constructor( private service: CommentService,private authService: AuthService){
     this.authService.user$.subscribe((user) => {
       this.user = user; 
       console.log(user);
     });
   }
+    */
 
   ngOnInit(): void {
     this.getBlogs();
   }
 
   getBlogs(): void {
+    /*
     this.service.getPosts().subscribe({
       next: (result: PagedResults<Post>) => {
         this.blogs = result.results.slice(0, 20);
@@ -44,5 +47,30 @@ export class LandingBlogComponent implements OnInit {
         console.log(err);
       }
     });
+    */
+   try{
+    for(let i = 0; i < 20; i++) {
+      const blog: Post = {
+        id: i + 1,
+        title: `Title ${i + 1}`,
+        description: `Description for blog post ${i + 1}`,
+        createdAt: new Date(),
+        imageUrl: this.genericImage,
+        status: Status.Published,  // Assuming Published status
+        userId: 1
+      };
+      
+      this.blogs.push(blog);
+     }
+  
+     if (this.blogs.length > 0) {
+        this.mainBlog = this.blogs[0];
+        this.blogs.shift();
+        this.mainBlog.imageUrl = "https://cdn.shopify.com/s/files/1/1083/2612/files/cr2_480x480.png?v=1721726734";
+      }
+
+   }catch{
+    alert("Error");
+   }
   }
 }
