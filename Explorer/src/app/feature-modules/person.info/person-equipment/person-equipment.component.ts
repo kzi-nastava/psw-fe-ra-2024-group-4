c656ef0fb16fdf96c25b264e25f4f6892ab9b988
Uros Stanisavljevic <avangerex@gmail.com>
2024-10-22 22:56:19 +0200

    Finish updating person equipment

--- Explorer/src/app/feature-modules/person.info/person-equipment/person-equipment.component.ts
+++ Explorer/src/app/feature-modules/person.info/person-equipment/person-equipment.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Equipment } from '../model/equipment.model';
 import { PersonInfoService } from '../person.info.service';
 import { PagedResults } from 'src/app/shared/model/paged-results.model';
+import { AuthService } from 'src/app/infrastructure/auth/auth.service';
+import { PersonInfo } from '../model/info.model';
 
 @Component({
   selector: 'xp-person-equipment',
@@ -10,12 +12,43 @@ import { PagedResults } from 'src/app/shared/model/paged-results.model';
 })
 export class PersonEquipmentComponent implements OnInit{
 	equipment: Equipment[] = []
-	selectedEquipment: Equipment[] = []
+	selectedEquipment: number[] = []
+  
+  person: PersonInfo = {
+    id : 0,
+    userId: 0,
+    name: '',
+    surname: '',
+    profilePicture: '',
+    biography: '',
+    motto: '',
+    equipment: [] as number[]
+  };
+  
+ constructor(private service: PersonInfoService, private authService: AuthService) {
 	
-constructor(private service: PersonInfoService) { }
+	  
+ }
 
   ngOnInit(): void {
+    this.authService.user$.subscribe(user => {
+      if (user && user.id) {
+        this.getPersonInfo(user.id);
+      }
+      });
     this.getEquipment();
+	  
+  }
+
+  getPersonInfo(personId: number): void {
+    this.service.getPersonInfo(personId).subscribe({
+      next: (result: PersonInfo) => {
+        this.person = result
+      },
+      error: (err) => {
+        console.error('Error fetching person info:', err);  
+      }
+    });
   }
 
   getEquipment(): void { 
@@ -27,4 +60,37 @@ constructor(private service: PersonInfoService) { }
       }
     })
   }
+
+
+  onCheckboxChange(equipmentId: number, event: Event): void {
+    const checkbox = (event.target as HTMLInputElement);
+    if (checkbox.checked) {
+      // Add the selected equipment ID to the list
+      this.selectedEquipment.push(equipmentId);
+      console.log("Id added " + equipmentId)
+    } else {
+      // Remove the deselected equipment ID from the list
+      this.selectedEquipment = this.selectedEquipment.filter(id => id !== equipmentId);
+      console.log("Id removed " + equipmentId)
+    }
+  }
+
+  saveChanges(): void {
+    this.person.equipment = [...this.selectedEquipment];
+    this.updatePersonEquipment();
+  }
+
+  updatePersonEquipment(): void {
+    
+    this.service.updatePersonInfo(this.person).subscribe({
+      next: () => {
+        console.log('Person equipment updated successfully');
+        console.log(this.person)
+      },
+      error: (err) => {
+        console.error('Error updating person equipment:', err);
+      }
+    });
+  }
 }
+
