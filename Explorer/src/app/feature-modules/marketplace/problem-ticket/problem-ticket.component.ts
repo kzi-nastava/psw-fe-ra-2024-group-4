4964e6cd7de2cbe8ce5796bf305c86ab2dc2b2a2
Damjan Ilic <damjanilic02@gmail.com>
2024-10-29 08:56:22 +0100

    fix: display full name and role on comments instead of id

--- Explorer/src/app/feature-modules/marketplace/problem-ticket/problem-ticket.component.ts
+++ Explorer/src/app/feature-modules/marketplace/problem-ticket/problem-ticket.component.ts
@@ -6,6 +6,10 @@ import { MarketplaceService } from '../marketplace.service';
 import { AuthService } from 'src/app/infrastructure/auth/auth.service';
 import { ProblemComment } from '../model/problem-comment.model';
 import { User } from 'src/app/infrastructure/auth/model/user.model';
+import { PersonInfo } from '../../person.info/model/info.model';
+import { PersonInfoService } from '../../person.info/person.info.service';
+import { TourService } from '../../tour-authoring/tour.service';
+import { Tour } from '../../tour-authoring/model/tour.model';
 
 @Component({
   selector: 'xp-problem-ticket',
@@ -17,9 +21,11 @@ export class ProblemTicketComponent implements OnInit {
   user: User | null = null;
   newCommentText: string = '';
   problem1: Problem; 
-  tourist: User | null = null;
-  author: User | null = null;
-  
+  users: {[key: number]: PersonInfo} = {}
+  userFullNames: {[key: number]: String} = {}
+  roles: {[key: number]: String} = {}
+  authorId: number = 0;
+
   newComment: ProblemComment = {
     problemId: 0,
     userId: 0,
@@ -43,44 +49,106 @@ export class ProblemTicketComponent implements OnInit {
       },
       {
         problemId: 5,
-        userId: 1,
+        userId: 2,
         text: 'TestKomentar 2',
         timeSent: new Date()
       },
     ],
   };
 
-  constructor(private service: MarketplaceService, private authService: AuthService) {
+  constructor(private service: MarketplaceService, private authService: AuthService, private personInfoService: PersonInfoService, private tourService: TourService) {
     this.authService.user$.subscribe((user) => {
       this.user = user; 
-      this.tourist = user;
-      console.log(user); 
+      //console.log(this.user);
     });
   }
 
   ngOnInit(): void {
+    this.setAuthorId();
     this.setRoles();
+    this.setNames();
   }
 
-  setRoles(){
-    if(this.user!==null){
-      if(this.user.role==='tourist'){
-        this.tourist = this.user;
-        this.getAuthor();
+  setAuthorId(){
+    this.problem.comments.forEach(comment => {
+      if(comment.userId !== this.problem.userId){
+        this.authorId = comment.userId;
       }
-      else{
-        this.author = this.user;
-        this.getTourist();
+    })
+  }
+
+
+  setNames(){
+    
+    //set names as tourist
+    if(this.user?.role==='tourist'){
+      //tourist name
+      this.setNameAsTourist(this.problem.userId);
+      //author
+      this.setNameAsTourist(this.authorId);
+    }
+    if(this.user?.role==='author'){
+      //tourist name
+      this.setNameAsAuthor(this.problem.userId);
+      //set author name
+      this.setNameAsAuthor(this.authorId);
+    }
+
+
+  }
+
+  setNameAsTourist(id: number){
+    this.personInfoService.getTouristInfo(id).subscribe({
+      next:(result: PersonInfo)=>{
+        this.users[result.userId] = result;
+        this.userFullNames[result.userId] = result.name + ' ' + result.surname;
+        //console.log(this.users);
+      },
+      error: () => {
+        console.log('error getting tourist info');
       }
+    })
+  }
+
+  setNameAsAuthor(id: number){
+    if(this.authorId !== 0){
+      this.personInfoService.getAuthorInfo(id).subscribe({
+        next:(result: PersonInfo)=>{
+          this.users[result.userId] = result;
+          this.userFullNames[result.userId] = result.name + ' ' + result.surname;
+          //console.log(this.users);
+        },
+        error: () => {
+          console.log('error getting author info');
+        }
+      })
+    }
+    else{
+      console.log('off sync ofc');
     }
   }
 
-  getAuthor(){
+ /* setAuthorId(): number{
+    var authorId = 0;
+    this.tourService.getTour(this.problem.tourId).subscribe({
+      next:(result: Tour)=>{
+        authorId = result.userId;
+        return authorId;
+      },
+      error:()=>{
+        console.log('Error getting tour');
+        return authorId;
+      }
+    })
+    return authorId;
+  }*/
+
+  setRoles(){
+    this.roles[this.problem.userId] = 'Tourist';
+    this.roles[this.authorId] = 'Author';
 
   }
-  getTourist(){
 
-  }
 
   onCommentInput(event: Event): void {
     const textarea = event.target as HTMLTextAreaElement;
@@ -115,7 +183,22 @@ export class ProblemTicketComponent implements OnInit {
         );
       }
       if(this.user.role ==='author'){
-        //postasauthor
+        this.service.postProblemCommentAsAuthor(this.newComment).subscribe(
+          (response: Problem) => {
+            console.log('Problem returned:', response);
+            //ovo obrisi /zakomentarisi
+            this.problem1 = response;
+            this.problem = response;
+            console.log(this.problem1);
+
+            this.newComment.text = ''; 
+          },
+          (error) => {
+            console.error('Error posting comment:', error);
+            console.log(this.newComment);
+            
+          }
+        );
       }
     }
   }
