50f5dfd0ee61ee69daa14a079566de3fd80bb03d
zopuu <puzovicveljko02@gmail.com>
2024-11-14 04:25:02 +0100

    ui: login form

--- Explorer/src/app/infrastructure/auth/login/login.component.ts
+++ Explorer/src/app/infrastructure/auth/login/login.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
 import { Login } from '../model/login.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'xp-login',
@@ -13,12 +14,13 @@ export class LoginComponent {
 
   constructor(
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private snackBar: MatSnackBar
   ) {}
 
   loginForm = new FormGroup({
     username: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
   });
 
   login(): void {
@@ -26,13 +28,21 @@ export class LoginComponent {
       username: this.loginForm.value.username || "",
       password: this.loginForm.value.password || "",
     };
-
+    const message = 'Pleas fill in all fields correctly.';
     if (this.loginForm.valid) {
       this.authService.login(login).subscribe({
         next: () => {
           this.router.navigate(['/']);
         },
       });
+    }
+    else{
+      this.snackBar.open(message,'Close', {
+       duration: 3000,
+       verticalPosition: 'bottom',
+       horizontalPosition: 'center',
+       panelClass: ['error-snackbar']
+      })
     }
   }
 }
--- Explorer/src/app/infrastructure/auth/registration/registration.component.ts
+++ Explorer/src/app/infrastructure/auth/registration/registration.component.ts
@@ -62,6 +62,6 @@ export class RegistrationComponent {
        horizontalPosition: 'center',
        panelClass: ['error-snackbar']
       })
-}
+    }
   }
 }
