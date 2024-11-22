4690a9767f8c6d0df83cff4d85f98dcb7d000a30
Maša Mastilović <masamastilovic@gmail.com>
2024-11-22 18:40:04 +0100

    feat: redesign

--- Explorer/src/app/feature-modules/tour-authoring/tour-search/tour-search.component.ts
+++ Explorer/src/app/feature-modules/tour-authoring/tour-search/tour-search.component.ts
@@ -1,17 +1,18 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { TourOverviewService } from '../tour-overview.service';
 import { PagedResults } from 'src/app/shared/model/paged-results.model';
 import { TourOverview } from '../model/touroverview.model';
 import { MatCheckboxChange } from '@angular/material/checkbox';
 import { TourTags } from '../model/tour.tags.model';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'xp-tour-search',
   templateUrl: './tour-search.component.html',
   styleUrls: ['./tour-search.component.css']
 })
-export class TourSearchComponent {
+export class TourSearchComponent implements OnInit {
 
   @Output() tourSearchResults = new EventEmitter<TourOverview[]>();
   latitude: number = 41.8992;
@@ -28,26 +29,13 @@ export class TourSearchComponent {
   
 
   constructor(private servis: TourOverviewService){}
+  ngOnInit(): void {
+     this.getAllTours();
+  }
 
-  tourSearchForm=new FormGroup({
-    distance: new FormControl(0, [Validators.required])
-  })
-
-  tourSearchForm1 = new FormGroup({
-    name: new FormControl('', [Validators.required]),
-    city: new FormControl(''),
-    country: new FormControl(''),
-    tags: new FormControl<number[]>([]),
-    distance: new FormControl(0, [Validators.required]),
-    difficulty: new FormControl(0, [Validators.required])
-});
-  
-  search(): void {
-    const distanceValue = this.tourSearchForm.value.distance ?? 0;
-    
-    this.servis.getToursByKeyPointLocation(this.longitude, this.latitude, distanceValue).subscribe({
+  getAllTours(): void {
+    this.servis.getAllWithoutReviews().subscribe({
       next: (data: PagedResults<TourOverview>) => {
-        // Emitujemo rezultat pretrage
         this.tourSearchResults.emit(data.results);
         console.log(data.results);
       },
@@ -57,9 +45,67 @@ export class TourSearchComponent {
     });
   }
 
+  tourSearchForm=new FormGroup({
+    distance: new FormControl(0),
+    name: new FormControl(''),
+  })
+
+  filterForm = new FormGroup({
+    tags: new FormControl<number[]>([]),
+    difficulty: new FormControl(0)
+});
+  
+  search(): void {
+    const distanceValue = this.tourSearchForm.value.distance ?? 0;
+    const nameValue = this.tourSearchForm.value.name ?? '';
+
+    if(distanceValue === 0 && nameValue === ''){
+      Swal.fire({
+        title: 'Warning!',
+        text: 'You must enter at least one search parameter!',
+        icon: 'warning',
+        confirmButtonText: 'OK'
+    });
+    this.getAllTours();
+    return;
+    }
+
+    if(distanceValue === 0){
+      this.servis.getAllWithoutReviews().subscribe({
+        next: (data: PagedResults<TourOverview>) => {
+           
+          // Emitujemo rezultat pretrage
+          this.tourSearchResults.emit(data.results);
+          console.log(data.results);
+        },
+        error: (err) => {
+          console.error('Error searching tours:', err);
+        }
+      });
+    }
+    
+    this.servis.getToursByKeyPointLocation(this.longitude, this.latitude, distanceValue).subscribe({
+      next: (data: PagedResults<TourOverview>) => {
+        let filteredResults = data.results;
+
+            if (nameValue) {
+                filteredResults = filteredResults.filter(tour =>
+                    tour.tourName.toLowerCase().includes(nameValue)
+                );
+            }
+        // Emitujemo rezultat pretrage
+        this.tourSearchResults.emit(filteredResults);
+        console.log(filteredResults);
+      },
+      error: (err) => {
+        console.error('Error searching tours:', err);
+      }
+    });
+  }
+
 
   onTagChange(event: MatCheckboxChange, index: number): void {
-    this.currentTags = this.tourSearchForm1.get('tags')?.value || [];
+    this.currentTags = this.filterForm.get('tags')?.value || [];
 
     if (event.checked) {
       this.currentTags.push(index);
@@ -70,7 +116,7 @@ export class TourSearchComponent {
         this.currentTags.splice(tagIndex, 1);
       }
     }
-    this.tourSearchForm1.get('tags')?.setValue(this.currentTags);
+    this.filterForm.get('tags')?.setValue(this.currentTags);
 
   }
   
@@ -81,8 +127,7 @@ export class TourSearchComponent {
 
   }
 
-  setLatitude(newLatitude: number): void{
-  
+  setLatitude(newLatitude: number): void{  
     this.latitude = newLatitude;
   }
 
