9060f54733421f951376cc0985ad757404035e55
zopuu <puzovicveljko02@gmail.com>
2024-11-23 20:51:20 +0100

    ui: adding equipment(admin)

--- Explorer/src/app/feature-modules/administration/equipment-form/equipment-form.component.ts
+++ Explorer/src/app/feature-modules/administration/equipment-form/equipment-form.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Inject, Input, OnChanges, OnInit, Output } fro
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Equipment } from '../model/equipment.model';
 import { AdministrationService } from '../administration.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'xp-equipment-form',
@@ -10,11 +11,11 @@ import { AdministrationService } from '../administration.service';
 })
 export class EquipmentFormComponent implements OnChanges {
 
-  @Output() equimpentUpdated = new EventEmitter<null>();
+  @Output() equipmentUpdated = new EventEmitter<null>();
   @Input() equipment: Equipment;
   @Input() shouldEdit: boolean = false;
 
-  constructor(private service: AdministrationService) {
+  constructor(private service: AdministrationService, private snackBar: MatSnackBar) {
   }
 
   ngOnChanges(): void {
@@ -30,23 +31,45 @@ export class EquipmentFormComponent implements OnChanges {
   });
 
   addEquipment(): void {
+    if (this.equipmentForm.invalid) {
+      this.snackBar.open('Please fill out all required fields.', 'Close', { duration: 3000 });
+      return;
+    }
     const equipment: Equipment = {
-      name: this.equipmentForm.value.name || "",
-      description: this.equipmentForm.value.description || "",
+      name: this.equipmentForm.value.name || '',
+      description: this.equipmentForm.value.description || '',
     };
     this.service.addEquipment(equipment).subscribe({
-      next: () => { this.equimpentUpdated.emit() }
+      next: () => {
+        this.snackBar.open('Equipment added successfully!', 'Close', { duration: 3000 });
+        this.equipmentForm.reset(); // Clear the form
+        this.equipmentUpdated.emit();
+      },
+      error: () => {
+        this.snackBar.open('Failed to add equipment.', 'Close', { duration: 3000 });
+      }
     });
   }
 
   updateEquipment(): void {
+    if (this.equipmentForm.invalid) {
+      this.snackBar.open('Please fill out all required fields.', 'Close', { duration: 3000 });
+      return;
+    }
     const equipment: Equipment = {
-      name: this.equipmentForm.value.name || "",
-      description: this.equipmentForm.value.description || "",
+      name: this.equipmentForm.value.name || '',
+      description: this.equipmentForm.value.description || '',
     };
     equipment.id = this.equipment.id;
     this.service.updateEquipment(equipment).subscribe({
-      next: () => { this.equimpentUpdated.emit();}
+      next: () => {
+        this.snackBar.open('Equipment updated successfully!', 'Close', { duration: 3000 });
+        this.equipmentForm.reset(); // Clear the form
+        this.equipmentUpdated.emit();
+      },
+      error: () => {
+        this.snackBar.open('Failed to update equipment.', 'Close', { duration: 3000 });
+      }
     });
   }
 }
