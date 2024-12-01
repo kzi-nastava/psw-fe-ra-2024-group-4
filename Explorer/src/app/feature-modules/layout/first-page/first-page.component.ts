9ce36aae1535f779cf6b95ef6705e46ad22a1eef
Nataša Radmilović <nata.radmilovic@gmail.com>
2024-12-01 20:41:24 +0100

    testing out browser text to speech

--- Explorer/src/app/feature-modules/layout/first-page/first-page.component.ts
+++ Explorer/src/app/feature-modules/layout/first-page/first-page.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'xp-first-page',
@@ -8,6 +8,20 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angul
 })
 export class FirstPageComponent {
   @Output() scrollToTarget = new EventEmitter<void>();
+  
+  uttr: SpeechSynthesisUtterance;
 
+  constructor(){
+    this.uttr = new SpeechSynthesisUtterance();
+    this.uttr.lang = 'en-US';
+  }
+
+
+  textToSpeech(): void {
+ 
+    this.uttr.text = "Travel tales";
+    window.speechSynthesis.speak(this.uttr);
+    
+  }
   
  }
