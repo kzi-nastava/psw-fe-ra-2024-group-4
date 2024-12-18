e14b2ffccb9d8f67993c24fd91e9c1cebe11b6ac
Ana Edelinski <aedelinski85@gmail.com>
2024-12-18 13:04:22 +0100

    Add animation

--- Explorer/src/app/feature-modules/layout/about-us/about-app/about-app.component.ts
+++ Explorer/src/app/feature-modules/layout/about-us/about-app/about-app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'xp-about-app',
   templateUrl: './about-app.component.html',
   styleUrls: ['./about-app.component.css']
 })
-export class AboutAppComponent {
-
+export class AboutAppComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('animate');
+          observer.unobserve(entry.target); 
+        }
+      });
+    });
+  
+    const observeElements = () => {
+      const images = document.querySelectorAll('.section-image, .section-content');
+      images.forEach(image => observer.observe(image));
+    };
+  
+    observeElements();
+  
+    const tabs = document.querySelectorAll('mat-tab-group');
+    tabs.forEach(tabGroup =>
+      tabGroup.addEventListener('click', () => {
+        setTimeout(() => observeElements(), 100); 
+      })
+    );
+  }
+  
 }
